d3283e190759d0a96ab0e2065fcf4b38d49e38bd
Keanu Kent B. Gargar <keanukent.dev@gmail.com>
2024-07-13 17:35:43 +0800

    Fix conditional styling in step node

--- src/components/ui/StepNode.vue
+++ src/components/ui/StepNode.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="relative">
-    <div class="p-3 text-white bg-accent-blue-500 border-[3px] border-blue-200 rounded-full leading-[0.5em]" :class="{
-      'bg-blue-200 text-blue-800/25': isDone
+    <div class="p-3 bg-accent-blue-500 border-[3px] border-blue-200 rounded-full leading-[0.5em]" :class="{
+      'bg-blue-200 text-blue-800/25': isDone,
+      'text-white': !isDone,
     }">
       {{ content }}
     </div>
-    <div class="absolute mt-1 font-medium text-slate-600 whitespace-nowrap left-1/2 -translate-x-1/2" :class="{
-      'text-slate-400': isDone
+    <div class="absolute mt-1 font-medium whitespace-nowrap left-1/2 -translate-x-1/2" :class="{
+      'text-slate-400': isDone,
+      'text-slate-600': !isDone
     }">{{ label }}</div>
   </div>
 </template>
